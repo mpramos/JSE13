1c2fda833bfe9d29c21deb9eb9dd8badc7c8fd10
maria <mpramos78915400@gmail.com>
2024-09-25 21:10:59 -0400

    dom primera parte

---
+++ modulo8/index1.js
@@ -0,0 +1,7 @@
+//!DOM
+// El documento HTML está estructurado como un objeto JavaScript.
+// Cada elemento HTML tiene diferentes propiedades que pueden ayudar a manipularlo.
+//  Es posible obtener, crear, agregar o eliminar elementos HTML usando JavaScript.
+// Revisa los ejemplos a continuación. La selección de un elemento HTML mediante
+// JavaScript es similar a la selección mediante CSS. Para seleccionar un elemento HTML,
+// usamos el nombre de la etiqueta, el identificador, el nombre de la clase u otros atributos.
